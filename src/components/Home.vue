a2c874c0edd0d21022c2b97787e8ceb6fc530c98
JuJu222 <japjustin@yahoo.com>
2022-03-12 00:42:21 +0700

    Updated home margin and h1

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative bg-white overflow-hidden">
-    <div class="max-w-7xl mx-auto">
+    <div class="max-w-7xl">
       <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
         <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
           <polygon points="50,0 100,0 50,100 0,100" />
@@ -34,6 +34,9 @@
     <Card class="m-5 z-10"/>
     <Card class="m-5 z-10"/>
     <Card class="m-5 z-10"/>
+  </div>
+  <div>
+    <h2 class="text-5xl">Skills</h2>
   </div>
 <!--  <div class="overflow-visible flex flex-row mb-96 items-center h-60">-->
 <!--    <Card class="m-5 z-10" />-->
